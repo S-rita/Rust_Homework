4c17f43a4c14a41e4bd2e31d8a3539e6575d2814
S-rita <139027632+S-rita@users.noreply.github.com>
2023-07-20 22:09:15 +0700

    Update names

--- HW2.2/tests/cli.rs
+++ HW2.2/tests/cli.rs
@@ -1,14 +1,14 @@
 use assert_cmd::Command;
 
 #[test]
+fn test_cf(){
+    let mut cmd = Command::cargo_bin("cf").unwrap();
+    cmd.arg("5").assert().success().stdout("5 degree Celcius converts to 41 degree Fahrenheit\n");
+}
+
+#[test]
 fn test_fc(){
     let mut cmd = Command::cargo_bin("fc").unwrap();
     cmd.arg("32").assert().success().stdout("32 degree Fahrenheit converts to 0 degree Celcius\n");
  
-}
-
-#[test]
-fn test_cf(){
-    let mut cmd = Command::cargo_bin("cf").unwrap();
-    cmd.arg("5").assert().success().stdout("5 degree Celcius converts to 41 degree Fahrenheit\n");
 }
