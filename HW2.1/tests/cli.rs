6ed835a60ef9ca059ecfa67b1f50da825abf7145
S-rita <139027632+S-rita@users.noreply.github.com>
2023-07-21 13:06:41 +0700

    Update cli.rs

--- HW2.1/tests/cli.rs
+++ HW2.1/tests/cli.rs
@@ -1,8 +1,16 @@
 use assert_cmd::Command;
+type TestResult = Result<(), Box<dyn std::error::Error>>;
 
 #[test]
-fn runs(){
+fn test_no_args() -> TestResult {
+    let mut cmd = Command::cargo_bin("area").unwrap();
+    cmd.assert().failure();
+    Ok(())
+}
+
+#[test]
+fn runs() -> TestResult {
     let mut cmd = Command::cargo_bin("area").unwrap();
     cmd.arg("25").assert().success().stdout("The area of a circle: 1963.5\n");
- 
+    Ok(())
 }
