0ee33f3a4cb41e3863333581f7f8da6d00e8799b
S-rita <139027632+S-rita@users.noreply.github.com>
2023-07-21 13:44:57 +0700

    Update cli.rs

--- HW2.3/tests/cli.rs
+++ HW2.3/tests/cli.rs
@@ -1,7 +1,30 @@
 use assert_cmd::Command;
+type TestResult = Result<(), Box<dyn std::error::Error>>;
 
 #[test]
-fn runs()  {
+fn no_arg() -> TestResult {
+    let mut cmd = Command::cargo_bin("list_players").unwrap();
+    cmd.arg("").arg("").assert().success().stdout("Player 1: \nPlayer 2: \n");
+    Ok(())
+}
+
+#[test]
+fn one_arg() -> TestResult {
+    let mut cmd = Command::cargo_bin("list_players").unwrap();
+    cmd.arg("Mike").arg("").assert().success().stdout("Player 1: Mike\nPlayer 2: \n");
+    Ok(())
+}
+
+#[test]
+fn two_args() -> TestResult {
     let mut cmd = Command::cargo_bin("list_players").unwrap();
     cmd.arg("Mike").arg("Leo").assert().success().stdout("Player 1: Mike\nPlayer 2: Leo\n");
+    Ok(())
+}
+
+#[test]
+fn three_args() -> TestResult {
+    let mut cmd = Command::cargo_bin("list_players").unwrap();
+    cmd.arg("Mike").arg("Leo").arg("Ralph").assert().success().stdout("Player 1: Mike\nPlayer 2: Leo\n");
+    Ok(())
 }
