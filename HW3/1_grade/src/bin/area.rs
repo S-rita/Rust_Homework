e22ce8f573e92db68d8efc388365c671ff5f6cba
S-rita <139027632+S-rita@users.noreply.github.com>
2023-07-28 01:25:47 +0700

    Add files via upload

---
+++ HW3/1_grade/src/bin/area.rs
@@ -0,0 +1,16 @@
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() < 2 {
+        panic!("No radius specified");
+    }
+
+    let x_arg: &String = &args[1];
+    let x: f32 = x_arg.parse().unwrap();
+
+println!("The area of a circle: {}", calculate_area(x));
+}
+
+fn calculate_area(r: f32) -> f32 {
+    const PI: f32 = 3.1416;
+    return PI * r * r;
+}
