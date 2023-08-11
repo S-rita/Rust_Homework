e51c7f9664ce5b86ca92d98be80ac9dd16477c91
S-rita <139027632+S-rita@users.noreply.github.com>
2023-08-11 13:26:35 +0700

    Update main.rs

--- HW4/3_arrow_loops/src/main.rs
+++ HW4/3_arrow_loops/src/main.rs
@@ -31,40 +31,28 @@ fn test_make_arrow1() {
 
 //3.2
 
-fn make_arrow2(v: usize) -> String {
-    let mut x = String::new();
+fn make_arrow2(x: usize) -> String {
+    let mut a = String::new();
 
-    for i in 0..v {
-        for _ in 0..(v - i - 1) {
-            x.push(' ');
-        }
-
-        for _ in 0..(2 * i + 1) {
-            x.push('*');
-        }
-
-        x.push('\n');
+    for i in 1..x {
+        a.push_str(&(" ".repeat(x-i)));
+        a.push_str(&("*".repeat(i)));
+        a.push('\n')
     }
 
-    for i in (0..v - 1).rev() {
-        for _ in 0..(v - i - 1) {
-            x.push(' ');
-        }
-
-        for _ in 0..(2 * i + 1) {
-            x.push('*');
-        }
-
-        x.push('\n');
+    for i in 0..x {
+        a.push_str(&(" ".repeat(i)));
+        a.push_str(&("*".repeat(x-i)));
+        a.push('\n')
     }
-
-    x
+    a
 }
 
 #[test]
 fn test_make_arrow2() {
-    assert_eq!(make_arrow2(3), "  *\n ***\n*****\n ***\n  *\n");
-    assert_eq!(make_arrow2(5), "    *\n   ***\n  *****\n *******\n*********\n *******\n  *****\n   ***\n    *\n");
+    assert_eq!(make_arrow2(0), "");
+    assert_eq!(make_arrow2(2), " *\n**\n *\n");
+    assert_eq!(make_arrow2(3), "  *\n **\n***\n **\n  *\n");
 }
 
 
@@ -95,33 +83,32 @@ fn test_make_arrow1_recursive () {
 }
 
 
-fn make_arrow2_recursive(v: &[usize]) -> String {
-    if v.len() == 0 {
-        String::new();
-    }
+fn make_arrow2_recursive(x: &[usize]) -> String {
+    if x.len() == 0 {
+        String::new()
+    } else {
+        let mut a = make_arrow2_recursive(&x[..x.len()-1]);
+        let n = x[x.len()-1];
 
-    let mut x = make_arrow2_recursive(&v[..v.len()-1]);
-    let n = v[v.len()-1];
+        for i in 1..n {
+            a.push_str(&(" ".repeat(n-i)));
+            a.push_str(&("*".repeat(i)));
+            a.push('\n')
+        }
 
-    for i in 1..n {
-        x.push_str(&("").repeat(n-i));
-        x.push_str(&("*").repeat(i));
-        x.push('\n')
+        for i in 0..n {
+            a.push_str(&(" ".repeat(i)));
+            a.push_str(&("*".repeat(n-i)));
+            a.push('\n')
+        }
+        a
     }
-
-    for i in 0..n {
-        x.push_str(&("").repeat(i));
-        x.push_str(&("*").repeat(n-i));
-        x.push('\n')
-    }
-    x
 }
 
 #[test]
 fn test_make_arrow2_recursive() {
-    assert_eq!(make_arrow2(3), "  *\n ***\n*****\n ***\n  *\n");
-    assert_eq!(make_arrow2(5), "    *\n   ***\n  *****\n *******\n*********\n *******\n  *****\n   ***\n    *\n");
+    assert_eq!(make_arrow2_recursive(&[]), "");
+    assert_eq!(make_arrow2_recursive(&[2]), " *\n**\n *\n");
+    assert_eq!(make_arrow2_recursive(&[3]), "  *\n **\n***\n **\n  *\n");
 }
-
-
 
