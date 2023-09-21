099076bd80dc3038e14f9d77dff0906457ff7824
S-rita <139027632+S-rita@users.noreply.github.com>
2023-09-21 17:28:52 +0700

    Update q1.rs

--- HW8/src/bin/q1.rs
+++ HW8/src/bin/q1.rs
@@ -1,39 +1,37 @@
-////////////// 1.1 //////////////
-fn to_polar(pt_list: &[(f64, f64)]) -> Vec<(f64, f64)> {
-    let mut PolarPoint = Vec::new();
+struct Point {
+    x: f64,
+    y: f64,
+}
 
-    for (x, y) in pt_list {
-        let r = (x.powi(2) + y.powi(2)).sqrt();
-        let t = (y / x).atan();
-        
-        PolarPoint.push((r, t));
-    }
-    PolarPoint
+struct PolarPoint {
+    r: f64,
+    t: f64,
+}
+
+////////////// 1.1 //////////////
+fn to_polar(point: &Point) -> PolarPoint {
+    let r = (point.x.powi(2) + point.y.powi(2)).sqrt();
+    let t = point.y.atan();
+    PolarPoint { r, t }
 }
 
 
 ////////////// 1.2 //////////////
-fn to_cartesian(pt_list: &[(f64, f64)]) -> Vec<(f64, f64)> {
-    let mut Point = Vec::new();
-
-    for (r, t) in pt_list {
-        let x = r * t.cos();
-        let y = r * t.sin();
-        Point.push((x, y));
-    }
-    Point
+fn to_cartesian(polar_point: &PolarPoint) -> Point {
+    let x = polar_point.r * polar_point.t.cos();
+    let y = polar_point.r * polar_point.t.sin();
+    Point { x, y }
 }
 
-////////////// Check //////////////
-fn main(){
-    let cartesian_points = [(1.0, 1.0), (2.0, 0.0)];
-    let polar_points = [(1.4142135623730951, 0.7853981633974483), (2.0, 0.0)];
 
-    let polar_result = to_polar(&cartesian_points);
-    let cartesian_result = to_cartesian(&polar_points);
+////////////// Check //////////////
+fn main() {
+    let cartesian_points = [Point { x: 1.0, y: 1.0 }, Point { x: 2.0, y: 0.0 }];
+    let polar_points = [PolarPoint { r: 1.4142135623730951, t: 0.7853981633974483 }, PolarPoint { r: 2.0, t: 0.0 }];
+
+    let polar_result = to_polar(&cartesian_points[0]);
+    let cartesian_result = to_cartesian(&polar_points[0]);
 
     println!("{:?}", polar_result);
     println!("{:?}", cartesian_result);
-
 }
-
